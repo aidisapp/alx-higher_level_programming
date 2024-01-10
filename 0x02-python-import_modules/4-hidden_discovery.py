47b85296b9ca7ffcb5ea007dfe754b379237d9b7
Idongesit Ekanem <aidisapp94@gmail.com>
2024-01-10 21:57:24 +0100

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 
-import importlib.util
-
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import importlib.util
 
     module_name = "hidden_4"
-    module_spec = importlib.util.spec_from_file_location(
-        module_name, "/mnt/c/Users/USER/Downloads/hidden_4.pyc"
-    )
-    module = importlib.util.module_from_spec(module_spec)
-    module_spec.loader.exec_module(module)
+    module = importlib.import_module(module_name)
 
-    filtered_names = [
-        name for name in dir(module) if not name.startswith("__")
-    ]
-
-    filtered_names.sort()
-    for name in filtered_names:
-        print(name)
+    names = dir(module)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
