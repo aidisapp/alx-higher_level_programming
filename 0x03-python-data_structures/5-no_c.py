f48486520e42ce41707ffe242cad2e7b65a7f947
Idongesit Ekanem <aidisapp94@gmail.com>
2024-01-11 15:42:58 +0100

    To update the solution for Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 
 def no_c(my_string):
@@ -7,3 +7,9 @@ def no_c(my_string):
         if character != 'c' and character != 'C':
             new_string += character
     return (new_string)
+
+
+if __name__ == "__main__":
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
