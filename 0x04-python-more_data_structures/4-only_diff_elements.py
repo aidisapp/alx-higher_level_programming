276791779dd3b8939eda99f94efd949071f7a1a6
Idongesit Ekanem <aidisapp94@gmail.com>
2024-01-16 16:47:31 +0100

    Still fixing output errors

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return(set_1.symmetric_difference(set_2))
+    """
+    Returns a set of elements present in only one set.
+    """
+    return (set_1.symmetric_difference(set_2))
