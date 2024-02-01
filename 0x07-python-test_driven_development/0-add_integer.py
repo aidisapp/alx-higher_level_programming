b4152e256d941a94d52a7bb64f459493489efb7d
Idongesit Ekanem <aidisapp94@gmail.com>
2024-02-01 13:11:44 +0100

    Update Task 0 Solution

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Module for adding two integers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int: The sum of a and b.
+    """
+    # Input validation and type casting
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+
+    # Type casting to integers
+    a = int(a)
+    b = int(b)
+
+    # Addition logic
+    result = a + b
+
+    return (result)
+
+# Test cases
+if __name__ == "__main__":
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+
+    # Exception cases
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
