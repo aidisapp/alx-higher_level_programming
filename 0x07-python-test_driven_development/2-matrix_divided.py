10b4f547e59d8909b04c089a14ffc58702cf9ebd
Idongesit Ekanem <aidisapp94@gmail.com>
2024-02-05 11:28:47 +0100

    Task 2 corrections

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,25 +8,27 @@ matrix_divided(matrix, divisor).
 
 def matrix_divided(matrix, divisor):
     """Divides all elements in the matrix by divisor"""
-    if type(matrix) is not list:
+    if not matrix or any(type(row) is not list for row in matrix):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats"
         )
+
     row_size = None
     for row in matrix:
-        if type(row) is not list:
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats")
         if row_size is None:
             row_size = len(row)
         elif row_size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
+
         for element in row:
             if type(element) is not int and type(element) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
+                raise TypeError("matrix must be a matrix \n"
+                                "(list of lists) of integers/floats")
+
     if type(divisor) is not int and type(divisor) is not float:
-        raise TypeError("divisor must be a number")
+        raise TypeError("div must be a number")
+
     if divisor == 0:
         raise ZeroDivisionError("division by zero")
+
     return [[round(element / divisor, 2) for element in row] for row in matrix]
