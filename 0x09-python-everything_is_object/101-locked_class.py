f0aec34a668039078475d4a68cc1f9a358b10b18
Idongesit Ekanem <aidisapp94@gmail.com>
2024-02-12 09:57:43 +0100

    Task 30 Solution

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
+"""This actually define a locked class"""
+
 
 class LockedClass:
     """Class that prevents the user from dynamically creating new instance attributes,
     except if the new instance attribute is called first_name.
     """
-    def __setattr__(self, name, value):
-        """Overrides the default setattr method to restrict attribute assignment.
-        
-        Args:
-            name (str): The name of the attribute being assigned.
-            value: The value to be assigned to the attribute.
-        
-        Raises:
-            AttributeError: If the attribute being assigned is not 'first_name'.
-        """
-        if name == 'first_name':
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+
+    __slots__ = ["first_name"]
