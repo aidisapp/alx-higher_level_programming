d6755d2306dcc3130a498b58d6675bb4cf70f29f
Idongesit Ekanem <aidisapp94@gmail.com>
2024-04-25 00:52:18 +0100

    Task 2 Solution

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in
+the states table where name matches the argument.
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    host = "localhost"
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    search_name = argv[4]
+
+    db_connection = MySQLdb.connect(host=host, user=user, passwd=password, db=database, port=3306)
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'".format(search_name))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db_connection.close()
