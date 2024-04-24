54a14be38864aa76ed6ece94a35a972d06d3ca83
Idongesit Ekanem <aidisapp94@gmail.com>
2024-04-24 14:47:13 +0100

    Task 1 Solution

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with
+N (upper N) from the database.
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    user = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
+
+    db_connection = MySQLdb.connect(user=user, passwd=password, db=database_name)
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'".format(state_name))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db_connection.close()
