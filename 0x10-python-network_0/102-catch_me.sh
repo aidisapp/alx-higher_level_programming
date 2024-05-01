d6931ce8d76089f92beb226ef2ce119c85643d29
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-01 18:43:08 +0100

    Task 8 & 9 Solutions

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This Script makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -X PUT -s 0.0.0.0:5000/catch_me --output /dev/null --header "Content-Type: text/plain" --data "You got me!"
