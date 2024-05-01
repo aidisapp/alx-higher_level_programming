00d6fa074e2d70d9ae8503c442353a2d4f8162e3
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-01 18:14:02 +0100

    Task 6 Update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,17 +15,17 @@ def find_peak(list_of_integers):
     beg = 0
     end = len(new_list) - 1
 
-    if new_list[beg] > new_list[beg + 1]:
+    if new_list[beg] >= new_list[beg + 1]:
         return new_list[beg]
-    if new_list[end] > new_list[end - 1]:
+    if new_list[end] >= new_list[end - 1]:
         return new_list[end]
 
-    mid = (beg + end) // 2
-    if new_list[mid - 1] < new_list[mid] and new_list[mid + 1] < new_list[mid]:
-        return new_list[mid]
-    if new_list[mid] < new_list[mid - 1]:
-        return find_peak(new_list[beg:mid + 1])
-    elif new_list[mid] < new_list[mid + 1]:
-        return find_peak(new_list[mid:end + 1])
-    else:
-        return new_list[beg]
+    while beg <= end:
+        mid = (beg + end) // 2
+        if new_list[mid] >= new_list[mid - 1] and new_list[
+                                                mid] >= new_list[mid + 1]:
+            return new_list[mid]
+        elif new_list[mid] < new_list[mid - 1]:
+            end = mid - 1
+        else:
+            beg = mid + 1
