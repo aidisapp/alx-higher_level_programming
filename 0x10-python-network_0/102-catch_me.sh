cbe1f7d63b7b76d29cb3aecc3cca479235bfb649
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-03 11:57:10 +0100

    Update on Task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This Script makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!"
-curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -L -H "Origin: sample.com"
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin: sample.com"
