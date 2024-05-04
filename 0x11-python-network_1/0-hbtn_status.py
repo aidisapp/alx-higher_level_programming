6994407c0daca6f5a97f067d37e81c17bbd4ee15
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-04 09:21:48 +0100

    Task 0 Solution

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+# This Python script to retrieve data from https://alx-intranet.hbtn.io/status
+# Utilizing the urllib package for HTTP requests
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
