73f8ac92c403d2afd7871bf8d956f6fbefc1c099
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-04 09:36:34 +0100

    Task 1 Solution

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a Python script to fetch the value of the X-Request-Id variable from a given URL's response header.
+
+Dependencies: urllib, sys
+"""
+
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader('x-Request-Id'))
