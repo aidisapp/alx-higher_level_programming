7291c8616ad10d8a26b669e54c68c01a2f7ab099
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-13 10:33:17 +0100

    Edit Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This Python script to send a POST request to a URL with an email parameter,
+and display the body of the response (decoded in utf-8).
+
+Requirements:
+- The URL and email must be passed as command-line arguments
+- Dependencies: urllib, sys
+"""
+
+from sys import argv
+from urllib.parse import urlencode 
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
+
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace")) 
