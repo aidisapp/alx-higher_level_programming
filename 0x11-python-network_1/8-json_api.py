482f7ebbeeda9563069a26422cd123ac878cc920
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-13 12:39:11 +0100

    Task 10 Solution error fixes

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,14 +14,15 @@ import requests
 
 
 if __name__ == "__main__":
-    letter = "" if len(argv) == 1 else argv[1]
-    req = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
+    letter = "" if len(argv) == 1 else argv[1
+    payload = {"q": letter}]
 
+     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = req.json()
-        if response:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-        else:
+        response = r.json()
+        if response == {}:
             print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
