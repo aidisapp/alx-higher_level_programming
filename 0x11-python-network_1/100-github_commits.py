37b6fc2d0050712db9aa86bcc79a8b235b71cfbd
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-13 17:33:59 +0100

    Task 10 PEP3 fixes

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     try:
         for i in range(10):
             print(f"{commits[i].get('sha')}: "
-                f"{commits[i].get('commit').get('author').get('name')}")
+                  f"{commits[i].get('commit').get('author').get('name')}")
     except IndexError:
         pass
