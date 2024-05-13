9048c76b417dc6574fa897f768c07e49112da20e
Idongesit Ekanem <aidisapp94@gmail.com>
2024-05-13 12:55:51 +0100

    Task 10 PEP3 fixes

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,9 +14,10 @@ if __name__ == "__main__":
     url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
     response = requests.get(url)
     commits = response.json()
-    
+
     try:
         for i in range(10):
-            print(f"{commits[i].get('sha')}: {commits[i].get('commit').get('author').get('name')}")
+            print(f"{commits[i].get('sha')}: {
+                commits[i].get('commit').get('author').get('name')}")
     except IndexError:
         pass
